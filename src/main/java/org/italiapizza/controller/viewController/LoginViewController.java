6fca2f457db94ce53d799ab4bc7215c44404e11c
Yoss <heyoarbo@gmail.com>
2026-06-03 12:06:52 -0600

    controladores ordenados por paquetes

--- src/main/java/org/italiapizza/controller/viewController/LoginViewController.java
+++ src/main/java/org/italiapizza/controller/viewController/LoginViewController.java
@@ -1,4 +1,4 @@
-package org.italiapizza.controller;
+package org.italiapizza.controller.viewController;
 
 import org.italiapizza.controller.exception.usuario.LoginException;
 import org.italiapizza.model.dto.Empleado;
@@ -7,6 +7,7 @@ import javafx.scene.control.Alert;
 import javafx.scene.control.Button;
 import javafx.scene.control.PasswordField;
 import javafx.scene.control.TextField;
+import org.italiapizza.controller.LoginController;
 
 /**
  * FXML Controller class
--- src/main/java/org/italiapizza/controller/viewController/RegistroUsuarioViewController.java
+++ src/main/java/org/italiapizza/controller/viewController/RegistroUsuarioViewController.java
@@ -2,7 +2,7 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/javafx/FXMLController.java to edit this template
  */
-package org.italiapizza.controller;
+package org.italiapizza.controller.viewController;
 
 import java.net.URL;
 import java.util.ResourceBundle;
@@ -13,7 +13,7 @@ import javafx.fxml.Initializable;
  *
  * @author ELLIN JV
  */
-public class MenuPrincipalViewController implements Initializable {
+public class RegistroUsuarioViewController implements Initializable {
 
     /**
      * Initializes the controller class.
